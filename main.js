ee2a5ed2ca7314f40e67f2857a7b56f34789fdb2
Arttutu <arthurtt08@gmail.com>
2022-11-04 20:36:59 -0300

    versal final

---
+++ main.js
@@ -0,0 +1,27 @@
+function creatGame(player1, player2, hour) {
+  return `
+      <li>
+        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+        <strong>${hour}</strong>
+        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+      </li>
+  `
+}
+let delay = -0.3
+function creatCard(date, day, games) {
+  delay = delay + 0.3
+  return `
+     <div class="card" style = "animation-delay: ${delay}s">
+      <h2>${date}<span>${day}</span></h2>
+            <ul>
+              ${games}
+            </ul>
+    </div> 
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+  //Concatena o texo
+  creatCard("24/11", "quinta", creatGame("brazil", "serbia", "16:00")) +
+  creatCard("28/11", "segunda", creatGame("brazil", "switzerland", "13:00")) +
+  creatCard("02/12", "sexta", creatGame("brazil", "cameroon", "16:00"))
