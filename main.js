a6bfbc8a3d647b5ee2412311b3b446556a846c1f
Arthur <arthurtt08@gmail.com>
2022-11-24 19:19:17 -0300

    resultado-jogo

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-function creatGame(player1, player2, hour) {
+function creatGame(player1, player2, hour, gol1, gol2) {
   return `
       <li>
         <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
         <strong>${hour}</strong>
         <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
       </li>
+      <ul>
+        <li>
+          <span> ${gol1} </span>
+          <span> x </span>
+          <span> ${gol2} </span>
+        </li>
+      </ul>
   `
 }
 let delay = -0.3
@@ -22,6 +29,10 @@ function creatCard(date, day, games) {
 
 document.querySelector("#cards").innerHTML =
   //Concatena o texo
-  creatCard("24/11", "quinta", creatGame("brazil", "serbia", "16:00")) +
-  creatCard("28/11", "segunda", creatGame("brazil", "switzerland", "13:00")) +
-  creatCard("02/12", "sexta", creatGame("brazil", "cameroon", "16:00"))
+  creatCard("24/11", "quinta", creatGame("brazil", "serbia", "16:00", 2, 0)) +
+  creatCard(
+    "28/11",
+    "segunda",
+    creatGame("brazil", "switzerland", "13:00", 2, 1)
+  ) +
+  creatCard("02/12", "sexta", creatGame("brazil", "cameroon", "16:00", 4, 0))
